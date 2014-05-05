b20c1330492e90b2bc569de0445183f35e5a814f
Jeff Mitzelfelt <jdmitz@gmail.com>
2014-05-05 10:51:33 -0500

    Clustered Features with Feature Markers
    All map services are Feature services.
    Each features type has its own icon and is grouped into its own
    clusters; although clusters are using the same iconography.

--- app/js/citizenMap.js
+++ app/js/citizenMap.js
@@ -9,21 +9,71 @@ citizenMap.maskUrl = 'http://geoservices.epa.illinois.gov/ArcGIS/rest/services/S
 citizenMap.actionLayers = [
 	{
 		name:'Medical Disposal Sites',
-		url: 'http://services1.arcgis.com/qI0WaD4k85ljbKGT/arcgis/rest/services/Medicine_Disposal_Locations/FeatureServer/0',
-		fields: [
-			'Name',
-			'Address',
-			'City',
-			'Telephone'
-		],
+		//url: 'http://services1.arcgis.com/qI0WaD4k85ljbKGT/arcgis/rest/services/Medicine_Disposal_Locations/FeatureServer/0',
+		url: 'http://epa084dgis01.iltest.illinois.gov:6080/arcgis/rest/services/Mitzelfelt/CitizenPrograms/FeatureServer/0',
 		bindMarker: function(geojson, marker){
-			marker.bindPopup("<h3>"+geojson.properties.Name+"</h3><p>"+geojson.properties.Address+"<br>"+ geojson.properties.City+",  IL</p><p>"+geojson.properties.Telephone+"</p>");
+			marker.bindPopup("<h3>Mecical "+geojson.properties.Name+"</h3><p>"+geojson.properties.Address+"<br>"+ geojson.properties.City+",  IL</p><p>"+geojson.properties.Telephone+"</p>");
+		},
+		createMarker: function(geojson, latlng){
+			return L.marker(latlng, {icon: L.icon({
+				iconUrl: 'img/medicalwastepin.png',
+				iconRetinaUrl: 'img/medicalwastepin.png',
+				iconSize: [32, 37],
+				iconAncor: [16, 37],
+				popupAncor:[0, -11]
+			})});
 		}
 	},
 	{
 		name:'Registered eWaste',
-		url: 'http://epa084pgis02.illinois.gov/arcgis/rest/services/OCR/ewastecollectsites_062613/MapServer',
-		layers: [0]
+		//url: 'http://epa084pgis02.illinois.gov/arcgis/rest/services/OCR/ewastecollectsites_062613/MapServer',
+		url: 'http://epa084dgis01.iltest.illinois.gov:6080/arcgis/rest/services/Mitzelfelt/CitizenPrograms/FeatureServer/3',
+		bindMarker: function(geojson, marker){
+			marker.bindPopup("<h3>eWaste "+geojson.properties.Company_Na+"</h3><p>"+geojson.properties.Address+"<br>"+ geojson.properties.City+",  IL</p><p>"+geojson.properties.Phone+"</p>");
+		},
+		createMarker: function(geojson, latlng){
+			return L.marker(latlng, {icon: L.icon({
+				iconUrl: 'img/ewasteeventpin.png',
+				iconRetinaUrl: 'img/ewasteeventpin.png',
+				iconSize: [32, 37],
+				iconAncor: [16, 37],
+				popupAncor:[0, -11]
+			})});
+		}
+	},
+	{
+		name:'Household Hazardous Waste Collection Sites',
+		//url: 'http://epa084pgis02.illinois.gov/arcgis/rest/services/OCR/ewastecollectsites_062613/MapServer',
+		url: 'http://epa084dgis01.iltest.illinois.gov:6080/arcgis/rest/services/Mitzelfelt/CitizenPrograms/FeatureServer/1',
+		bindMarker: function(geojson, marker){
+			marker.bindPopup("<h3>Hazardous " + geojson.properties.sponsor + "</h3><p>" + geojson.properties.address + "<br>" + geojson.properties.city + ",  IL</p><p>" + geojson.properties.date + "</p>");
+		},
+		createMarker: function(geojson, latlng){
+			return L.marker(latlng, {icon: L.icon({
+				iconUrl: 'img/householdhazardpin.png',
+				iconRetinaUrl: 'img/householdhazardpin.png',
+				iconSize: [32, 37],
+				iconAncor: [16, 37],
+				popupAncor:[0, -11]
+			})});
+		}
+	},
+	{
+		name:'VIM Testing Stations',
+		//url: 'http://geoservices.epa.illinois.gov/arcgis/rest/services/OCR/metroeastvts_edit_092513/FeatureServer/0',
+		url: 'http://epa084dgis01.iltest.illinois.gov:6080/arcgis/rest/services/Mitzelfelt/CitizenPrograms/FeatureServer/2',
+		bindMarker: function(geojson, marker){
+			marker.bindPopup("<h3>VIM "+geojson.properties.name+"</h3><p>"+geojson.properties.address+"<br>"+ geojson.properties.city+",  IL</p><p>"+geojson.properties.telephone+"</p><p>"+geojson.properties.operationHours + "</p>");
+		},
+		createMarker: function(geojson, latlng){
+			return L.marker(latlng, {icon: L.icon({
+				iconUrl: 'img/bluevehiclepin.png',
+				iconRetinaUrl: 'img/bluevehiclepin.png',
+				iconSize: [32, 37],
+				iconAncor: [16, 37],
+				popupAncor:[0, -11]
+			})});
+		}
 	}
 ];
 
@@ -43,10 +93,23 @@ citizenMap.maskLayer = new L.esri.DynamicMapLayer(citizenMap.maskUrl,{
 citizenMap.featureLayers = [
 	new L.esri.ClusteredFeatureLayer(citizenMap.actionLayers[0].url,{
 		cluster: new L.MarkerClusterGroup(),
+		createMarker: citizenMap.actionLayers[0].createMarker,
 		onEachMarker: citizenMap.actionLayers[0].bindMarker
 	}).addTo(citizenMap.map),
-	new L.esri.DynamicMapLayer(citizenMap.actionLayers[1].url,{
-		layers: citizenMap.actionLayers[1].layers
+	new L.esri.ClusteredFeatureLayer(citizenMap.actionLayers[1].url,{
+		cluster: new L.MarkerClusterGroup(),
+		createMarker: citizenMap.actionLayers[1].createMarker,
+		onEachMarker: citizenMap.actionLayers[1].bindMarker
+	}).addTo(citizenMap.map),
+	new L.esri.ClusteredFeatureLayer(citizenMap.actionLayers[2].url,{
+		cluster: new L.MarkerClusterGroup(),
+		createMarker: citizenMap.actionLayers[2].createMarker,
+		onEachMarker: citizenMap.actionLayers[2].bindMarker
+	}).addTo(citizenMap.map),
+	new L.esri.ClusteredFeatureLayer(citizenMap.actionLayers[3].url,{
+		cluster: new L.MarkerClusterGroup(),
+		createMarker: citizenMap.actionLayers[3].createMarker,
+		onEachMarker: citizenMap.actionLayers[3].bindMarker
 	}).addTo(citizenMap.map)
 	];
 
